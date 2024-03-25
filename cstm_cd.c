637f4907954935662b665be89722f888b891549f
deniz <oezdemir.de@gmail.com>
2024-03-25 11:29:04 +0100

    cstm_get_envpi added

--- cstm_cd.c
+++ cstm_cd.c
@@ -6,11 +6,46 @@
 /*   By: denizozd <denizozd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/22 14:35:29 by denizozd          #+#    #+#             */
-/*   Updated: 2024/03/22 15:25:00 by denizozd         ###   ########.fr       */
+/*   Updated: 2024/03/25 11:27:45 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+/* custom getenv for the parsed environment variables in the prompt struct */
+char *cstm_get_envp(t_prompt *prompt, char *name)
+{
+	int	i;
+	int l;
+	char	*str;
+	char	*env_var;
+
+	i = 0;
+	if (!name)
+		return (NULL);
+	str = ft_strjoin(name, "=");
+	if (!str)
+		return (NULL);
+	l = ft_strlen(str);
+	while(prompt->envp[i])
+	{
+		if (!ft_strncmp(prompt->envp[i], str, l))
+		{
+			env_var = ft_strdup(prompt->envp[i] + l);
+			if (str)
+				free(str);
+			return (env_var);
+		}
+	}
+	if (str)
+		free(str);
+	return (NULL);
+}
+
+int	go_home(t_cmddat *cmd_data)
+{
+
+}
 
 int cstm_cd(t_cmddat *cmd_data)
 {
@@ -19,7 +54,7 @@ int cstm_cd(t_cmddat *cmd_data)
 	if (cmd_data->full_command)
 	{
 		if (!cmd_data->full_command[1])
-			return(go_home()); //write
+			return(go_home(void)); //write
 		else if(ft_strcmp(cmd_data->full_command[1], "-"))
 			return(go_back()); //write
 	}
