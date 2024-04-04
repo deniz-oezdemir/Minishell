3663eca549eea9f2f4ee9e85ad4dda2aa9c40b22
deniz <oezdemir.de@gmail.com>
2024-04-04 15:40:25 +0200

    spo cstm_lst_functions

--- list_functions.c
+++ list_functions.c
@@ -6,7 +6,41 @@
 /*   By: denizozd <denizozd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 13:08:23 by denizozd          #+#    #+#             */
-/*   Updated: 2024/04/04 13:08:24 by denizozd         ###   ########.fr       */
+/*   Updated: 2024/04/04 15:32:42 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "minishell.h"
+
+void	cstm_lstiter(t_node *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst)
+	{
+		f(lst->data);
+		lst = lst->next;
+	}
+}
+
+t_node	*cstm_lstlast(t_node *lst)
+{
+	if (!lst)
+		return (NULL);
+	while (lst->next)
+		lst = lst->next;
+	return (lst);
+}
+
+int	cstm_lstsize(t_node*lst)
+{
+	int	i;
+
+	i = 0;
+	while (lst)
+	{
+		lst = lst->next;
+		i++;
+	}
+	return (i);
+}
