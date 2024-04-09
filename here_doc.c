13febced4d30fb590c911856b43aab5f3ad5d2ae
Eleonore <eleonorecarlier@hotmail.be>
2024-04-09 17:38:39 +0200

    spo

--- here_doc.c
+++ here_doc.c
@@ -6,7 +6,7 @@
 /*   By: ecarlier <ecarlier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 15:25:27 by ecarlier          #+#    #+#             */
-/*   Updated: 2024/04/05 18:29:25 by ecarlier         ###   ########.fr       */
+/*   Updated: 2024/04/09 17:37:56 by ecarlier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,32 @@
 		line = readline(HERE_DOC_PROMPT);
 		signals_non_interactive();
 		/
-void	get_here_doc(t_prompt *prompt, t_cmddat *cmd_struct, int i)
-{
-	//
+// */
 
 
-}
-*/
+
+/*COMPLETLY LOST*/
+// void	get_here_doc(t_prompt *prompt, t_cmddat *cmd_struct, int i)
+// {
+
+// 	char *line;
+
+// 	line = NULL;
+// 	while (1)
+// 	{
+// 		signals_interactive();
+// 		line = readline("> ");
+// 		if (!line)
+// 			print_err_msg("warning", "here-document delimited by end-of-file");
+
+
+// 	}
+
+// }
+
+
+
+// void	start_here_doc(t_prompt *prompt, t_cmddat *cmd_strc, int i)
+// {
+
+// }
