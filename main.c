f59f7bfec3b5670df5f1d1b9776902b00c0854b2
deniz <oezdemir.de@gmail.com>
2024-04-21 19:32:31 +0200

    fix

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: denizozd <denizozd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/11 18:56:55 by ecarlier          #+#    #+#             */
-/*   Updated: 2024/04/21 18:30:47 by denizozd         ###   ########.fr       */
+/*   Updated: 2024/04/21 19:12:20 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,7 +49,8 @@ void	launch_minishell(t_prompt *prompt)
 		if (cstm_lstsize(prompt->cmd_list) > 1 && prompt->stop == 0)
 			pipe_infile_outfile(prompt->cmd_list); //@Deniz: not gc'ed within pipe_infile_outfile as does not leak
 		//print_cmd_list(prompt->cmd_list);
-		execute_cmds(prompt); //maybe add stop check
+		if (!prompt->stop)
+			execute_cmds(prompt);
 		prompt->cmd_list = NULL; //this works but why @Leo
 	}
 }
