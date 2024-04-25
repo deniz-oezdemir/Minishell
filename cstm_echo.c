28fb5328b28152a4cce0a8620cdd7b090e030071
deniz <oezdemir.de@gmail.com>
2024-04-25 11:43:57 +0200

    can not fix redirects with echo without breaking more, resetting echo

--- cstm_echo.c
+++ cstm_echo.c
@@ -6,7 +6,7 @@
 /*   By: denizozd <denizozd@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/14 13:50:56 by denizozd          #+#    #+#             */
-/*   Updated: 2024/04/22 17:57:45 by denizozd         ###   ########.fr       */
+/*   Updated: 2024/04/25 11:35:15 by denizozd         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,13 +21,18 @@ int	cstm_echo(t_cmddat *cmd_data)
 	i = 1;
 	if (cmd_data == NULL)
 		return (1);
-	if (cmd_data->infile != 0) //if pipe before echo - but could also be redirects!! also true for export! -> need pipe before flag? ///@Deniz: instead IDEA: function that iterates through command list and assigns every cmd_struct it's position in the cmd_list -> if echo is not the first cmd, there is a piupe infront of it
+	/*if (cmd_data->prompt->nbr_cmds > 1 && cmd_data->lst_pos + 1 == cmd_data->prompt->nbr_cmds) //if there are multiple commands and echo is last cmd
+	{
+		close(cmd_data->infile);
+		return (0);
+	}*/
+	/*if (cmd_data->infile != 0) //if pipe before echo - but could also be redirects!! also true for export! -> need pipe before flag? ///@Deniz: instead IDEA: function that iterates through command list and assigns every cmd_struct it's position in the cmd_list -> if echo is not the first cmd, there is a piupe infront of it
 	{
 		//close(cmd_->infile);
 		//cmd- = 1;
 		exitstatus = 1; // or return 1?
 		return (0);
-	}
+	}*/
 	if (cmd_data->full_command[1] == NULL)
 		return (ft_putstr_fd("\n", cmd_data->outfile), 1);
 	if (!ft_strcmp(cmd_data->full_command[1], "-n")) //different
